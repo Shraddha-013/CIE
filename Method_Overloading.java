1836e9982d979889b68d46d1fbcdd9d902cae9d8
Shraddha-013 <sjshraddha13@gmail.com>
2025-04-15 09:56:11 +0530

    Saved

---
+++ Method_Overloading.java
@@ -0,0 +1,25 @@
+package Overloading;
+
+public class Method_Overloading {
+     int add(int a, int b) {
+        int sum= a + b;
+        System.out.println("Sum is: " + sum);
+        return sum;
+      }
+      int add(int a,double b) {
+     double sum= a + b;
+        System.out.println("Sum is: " + sum);
+        return (int) sum;
+      }
+      int add(int a, int b, int c) {
+        int sum= a + b + c;
+        System.out.println("Sum is: " + sum);
+        return sum;
+      }
+        public static void main(String[] args) {
+            Method_Overloading obj = new Method_Overloading();
+            obj.add(1, 2);
+            obj.add(1, 2.5);
+            obj.add(1, 2, 3);
+        }
+}
